b4f5a71a9d99ca81f29da2bf2e6fc79754113fd0
LEONARDO HENRIQUE APARECIDO <78cheater@gmail.com>
2024-06-13 13:03:12 -0300

    Qulauere rwrfsdf

---
+++ ArraysVetor/Vetor2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysVetor
+{
+    internal class Vetor2
+    {
+        public static void Main(string[] args)
+        {// Create an array of four elements and add values right away
+            string[] car = new string[4] { "Volvo", "", "", "" };
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine(car[3]);//sdsdsdsdsdsd
+            }
+        }
+    }
+}
